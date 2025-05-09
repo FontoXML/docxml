188cdba27b20fb06e902ac8d348cb10c35b6d436
Gabe Webb <gabe.webb@liones.nl>
2025-05-09 13:18:32 +0200

    Add file for creating footnoteReference in document.

---
+++ src/classes/Footnote.test.ts
@@ -0,0 +1,15 @@
+import { describe, it } from "std/testing/bdd";
+import { FootnoteReference } from "./Footnote.ts";
+import * as slimdom from 'slimdom'; 
+import { evaluateXPathToString } from "../utilities/xquery.ts";
+
+describe(`Test footnote references in documents`, () => { 
+    
+    it('Generates an ooxml footnote reference from a node', () => { 
+        const footnoteReference = new FootnoteReference({id: 1}); 
+        const referenceNode = footnoteReference.toNode();  
+        console.log(evaluateXPathToString(`//*`, referenceNode)); 
+        
+    
+    })
+})
--- src/files/FootnotesXml.ts
+++ src/files/FootnotesXml.ts
@@ -3,9 +3,13 @@ import { Archive } from "../classes/Archive.ts";
 import { XmlFile } from '../classes/XmlFile.ts';
 import { FileMime } from '../enums.ts';
 import { NumberMap } from "../classes/NumberMap.ts";
+import * as slimdom from 'slimdom'; 
 import * as path from 'std/path'; 
-import { evaluateXPathToArray } from "../utilities/xquery.ts";
-import { QNS } from "../utilities/namespaces.ts";
+import { evaluateXPathToArray, evaluateXPathToNodes } from "../utilities/xquery.ts";
+import { ALL_NAMESPACE_DECLARATIONS, QNS } from "../utilities/namespaces.ts";
+import { RelationshipsXml } from './RelationshipsXml.ts'; 
+import { ContentTypesXml } from "./ContentTypesXml.ts";
+import { create } from "../utilities/dom.ts";
 
 export enum FootnoteType { 
 	"separator", 
@@ -36,40 +40,67 @@ export class FootnotesXml extends XmlFile {
 		this.#footnotes.set(id, newFootnote); 
 		return newFootnote; 
 	}
+
+	/**
+	 * 
+	 *  Creates an OOXML representation of the FootnotesXml. 
+	 */
+	protected override async toNode(): Promise<Document> {
+		return create(`
+			<w:footnotes ${ALL_NAMESPACE_DECLARATIONS}>
+				for $footnote in array:flatten($footnotes)
+				return (
+					element w:footnote {
+						attribute w:type { $type }
+					}
+				)
+			</w:footnotes>
+		`,
+		{
+			footnotes: await Promise.all(
+				this.#footnotes.array().map(async (footnote) => ({ 
+					type: footnote.type,
+					id: footnote.id,
+					content: await Promise.all(
+						(footnote.content).map((p) => p.toNode([]))
+					),
+				}))
+			)
+		},
+		true);
+	}
 	/**
 	 * Instantiate this class by looking at the DOCX XML for it.
 	 */
 	public static override async fromArchive(
 		archive: Archive,
-		location: string,
+		location: string
 	): Promise<FootnotesXml>{
-		
-		const relsLocation = `${path.dirname(location)}/${path.basename(location)}`; 
-		const relationships = archive.hasFile(relsLocation) 
-			? await archive.readXml(location) 
-			: null;
-			
-		const footnotesFromArchive = new this(location); 
-		
-		if (relationships && relationships != null) { 
-			evaluateXPathToArray(
-				`array {
-					//${QNS.w}footnote/map { 
-						"id" : @${QNS.w}id/number(),
-						"content": array { ./${QNS.w}p }, 
-						"type": @${QNS.w}type/string()
-					}
-				}`,
-			relationships).forEach((n) => (
-				footnotesFromArchive.add(
-					n.forEach((p) => {
-						Paragraph.fromNode(n, {archive, relationships})
-					})
-				)	
-			))
-		}
-		else {
-			return footnotesFromArchive
+		const contentType = new ContentTypesXml(location); 
+		const relsLocation = `${path.dirname(location)}/${path.basename(location)}`;
+		const inst = new this(location); 
+		if (archive.hasFile(relsLocation)) { 
+			const relsDom = await archive.readXml(location); 
+			const relationships =  await RelationshipsXml.fromArchive(archive, contentType, relsLocation);
+			if (relationships && relationships != null) { 
+				evaluateXPathToArray(
+					`array {
+						//${QNS.w}footnote/map { 
+							"id" : @${QNS.w}id/number(),
+							"content": array { ./${QNS.w}p }, 
+							"type": @${QNS.w}type/string()
+						}
+					}`,
+					relsDom
+				).forEach((footnote) => {
+					inst.add(footnote.content.map((f: Node) => Paragraph.fromNode(f, {archive, relationships})), footnote.type)
+				})
+			}
+			// console.log(inst.#footnotes);
+			return inst;
+		} else { 
+			return inst;
 		}
 	}
+
 }
