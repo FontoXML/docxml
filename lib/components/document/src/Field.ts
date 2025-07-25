3c2c76c542592ae7aaec95df0f6ba23972bc7889
Inés Nieto <inieto@rws.com>
2025-07-25 14:49:42 +0200

    DEV-24696 Add Insertion as paragraph and field child

--- lib/components/document/src/Field.ts
+++ lib/components/document/src/Field.ts
@@ -118,6 +118,7 @@ export class Field extends Component<FieldProps, FieldChild> {
 					${QNS.w}r |
 					${QNS.w}hyperlink |
 					${QNS.w}del |
+					${QNS.w}ins |
 					${QNS.w}commentRangeStart |
 					${QNS.w}commentRangeEnd |
 					${QNS.w}bookmarkRangeStart |
