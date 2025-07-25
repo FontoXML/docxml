db33b003828a4b3ddca01ea63c94bec758338b63
Inés Nieto <inieto@rws.com>
2025-07-25 16:10:49 +0200

    DEV-24696 Add comment

--- lib/components/document/src/Text.ts
+++ lib/components/document/src/Text.ts
@@ -87,6 +87,9 @@ export class Text extends Component<TextProps, TextChild> {
 	 * Creates an XML DOM node for this component instance.
 	 */
 	public override async toNode(ancestry: ComponentAncestor[]): Promise<Node> {
+		// It's necessary to verify if the text run content node is inside a <del> element,
+		// because according to the schema, the text node should be <delText> instead of <t>
+		// https://c-rex.net/samples/ooxml/e1/Part4/OOXML_P4_DOCX_del_topic_ID0ESZZV.html
 		const asTextDeletion = ancestry.some(
 			(ancestor) => ancestor instanceof Deletion
 		);
