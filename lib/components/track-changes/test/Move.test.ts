c18a584e64daa8babc7d67d585d6f4b31ec27772
Inés Nieto <inieto@rws.com>
2025-07-29 08:57:32 +0200

    Make author and date optional and add tests

--- lib/components/track-changes/test/Move.test.ts
+++ lib/components/track-changes/test/Move.test.ts
@@ -29,10 +29,51 @@ describe('Move content in track changes...', () => {
 		emptyContext
 	);
 
+	const moveToNodeWithoutAuthor = create(
+		`<w:p xmlns:w="${NamespaceUri.w}">
+			<w:moveTo w:id="0" w:date="${date.toISOString()}">
+				<w:r>
+					<w:t xml:space="preserve">This is a paragraph</w:t>
+				</w:r>
+			</w:moveTo>
+		</w:p>`,
+		emptyContext
+	);
+	const moveToNodeWithoutDate = create(
+		`<w:p xmlns:w="${NamespaceUri.w}">
+			<w:moveTo w:id="0" w:author="Gabe">
+				<w:r>
+					<w:t xml:space="preserve">This is a paragraph</w:t>
+				</w:r>
+			</w:moveTo>
+		</w:p>`,
+		emptyContext
+	);
+
 	const moveFromNode = create(
 		`<w:moveFrom xmlns:w="${
 			NamespaceUri.w
 		}" w:id="1" w:author="Angel" w:date="${date.toISOString()}">
+            <w:r>
+                <w:t xml:space="preserve">This is a moveFrom node.</w:t>
+            </w:r>
+		</w:moveFrom>`,
+		emptyContext
+	);
+
+	const moveFromNodeWithoutAuthor = create(
+		`<w:moveFrom xmlns:w="${
+			NamespaceUri.w
+		}" w:id="1" w:date="${date.toISOString()}">
+            <w:r>
+                <w:t xml:space="preserve">This is a moveFrom node.</w:t>
+            </w:r>
+		</w:moveFrom>`,
+		emptyContext
+	);
+
+	const moveFromNodeWithoutDate = create(
+		`<w:moveFrom xmlns:w="${NamespaceUri.w}" w:id="1" w:author="Angel">
             <w:r>
                 <w:t xml:space="preserve">This is a moveFrom node.</w:t>
             </w:r>
@@ -45,6 +86,32 @@ describe('Move content in track changes...', () => {
 			<w:pPr>
 				<w:rPr>
 					<w:moveTo w:id="1" w:author="Luis" w:date="${date.toISOString()}" />
+				</w:rPr>
+			</w:pPr>
+			<w:r>
+				<w:t>Hello this is some text.</w:t>
+			</w:r>
+		</w:p>`
+	);
+
+	const moveToAsPropNodeWithoutAuthor = create(
+		`<w:p xmlns:w="${NamespaceUri.w}">
+			<w:pPr>
+				<w:rPr>
+					<w:moveTo w:id="1" w:date="${date.toISOString()}" />
+				</w:rPr>
+			</w:pPr>
+			<w:r>
+				<w:t>Hello this is some text.</w:t>
+			</w:r>
+		</w:p>`
+	);
+
+	const moveToAsPropNodeWithoutDate = create(
+		`<w:p xmlns:w="${NamespaceUri.w}">
+			<w:pPr>
+				<w:rPr>
+					<w:moveTo w:id="1" w:author="Luis" />
 				</w:rPr>
 			</w:pPr>
 			<w:r>
@@ -80,11 +147,52 @@ describe('Move content in track changes...', () => {
 		)
 	);
 
+	const moveToObjectWithoutDate = new Paragraph(
+		{ style: null },
+		new Move(
+			{
+				id: 0,
+				author: 'Gabe',
+				type: 'to',
+			},
+			new Text({}, 'This is a paragraph')
+		)
+	);
+
+	const moveToObjectWithoutAuthor = new Paragraph(
+		{ style: null },
+		new Move(
+			{
+				id: 0,
+				date: date,
+				type: 'to',
+			},
+			new Text({}, 'This is a paragraph')
+		)
+	);
+
 	// Testing a move as a stand-alone object
 	const moveFromObject = new Move(
 		{
 			id: 1,
 			date: date,
+			author: 'Angel',
+			type: 'from',
+		},
+		new Text({}, 'This is a moveFrom node.')
+	);
+
+	const moveFromObjectWithoutAuthor = new Move(
+		{
+			id: 1,
+			date: date,
+			type: 'from',
+		},
+		new Text({}, 'This is a moveFrom node.')
+	);
+	const moveFromObjectWithoutDate = new Move(
+		{
+			id: 1,
 			author: 'Angel',
 			type: 'from',
 		},
@@ -97,6 +205,32 @@ describe('Move content in track changes...', () => {
 				move: {
 					author: 'Luis',
 					date: date,
+					type: 'to',
+					id: 1,
+				},
+			},
+		},
+		new Text({}, 'Hello this is some text.')
+	);
+
+	const moveToAsPropObjectWithoutAuthor = new Paragraph(
+		{
+			pilcrow: {
+				move: {
+					date: date,
+					type: 'to',
+					id: 1,
+				},
+			},
+		},
+		new Text({}, 'Hello this is some text.')
+	);
+
+	const moveToAsPropObjectWithoutDate = new Paragraph(
+		{
+			pilcrow: {
+				move: {
+					author: 'Luis',
 					type: 'to',
 					id: 1,
 				},
@@ -120,8 +254,35 @@ describe('Move content in track changes...', () => {
 	);
 
 	const newMoveTo = Paragraph.fromNode(moveToNode, emptyContext);
+	const newMoveToWithoutAuthor = Paragraph.fromNode(
+		moveToNodeWithoutAuthor,
+		emptyContext
+	);
+	const newMoveToWithoutDate = Paragraph.fromNode(
+		moveToNodeWithoutDate,
+		emptyContext
+	);
+
 	const newMoveFrom = Move.fromNode(moveFromNode, emptyContext);
+	const newMoveFromWithoutAuthor = Move.fromNode(
+		moveFromNodeWithoutAuthor,
+		emptyContext
+	);
+	const newMoveFromWithoutDate = Move.fromNode(
+		moveFromNodeWithoutDate,
+		emptyContext
+	);
+
 	const newMoveToAsProp = Paragraph.fromNode(moveToAsPropNode, emptyContext);
+	const newMoveToAsPropWithoutAuthor = Paragraph.fromNode(
+		moveToAsPropNodeWithoutAuthor,
+		emptyContext
+	);
+	const newMoveToAsPropWithoutDate = Paragraph.fromNode(
+		moveToAsPropNodeWithoutDate,
+		emptyContext
+	);
+
 	const newMoveFromAsProp = Paragraph.fromNode(
 		moveFromAsPropNode,
 		emptyContext
@@ -129,10 +290,23 @@ describe('Move content in track changes...', () => {
 
 	it('turns node into expected Move objects', () => {
 		expect(newMoveTo).toEqual(moveToObject);
+		expect(newMoveToWithoutAuthor).toEqual(moveToObjectWithoutAuthor);
+		expect(newMoveToWithoutDate).toEqual(moveToObjectWithoutDate);
+
 		expect(newMoveFrom).toEqual(moveFromObject);
+		expect(newMoveFromWithoutAuthor).toEqual(moveFromObjectWithoutAuthor);
+		expect(newMoveFromWithoutDate).toEqual(moveFromObjectWithoutDate);
+
 		expect(newMoveToAsProp.props.pilcrow?.move).toEqual(
 			moveToAsPropObject.props.pilcrow?.move
 		);
+		expect(newMoveToAsPropWithoutAuthor.props.pilcrow?.move).toEqual(
+			moveToAsPropObjectWithoutAuthor.props.pilcrow?.move
+		);
+		expect(newMoveToAsPropWithoutDate.props.pilcrow?.move).toEqual(
+			moveToAsPropObjectWithoutDate.props.pilcrow?.move
+		);
+
 		expect(newMoveFromAsProp.props.pilcrow?.move).toEqual(
 			moveFromAsPropObject.props.pilcrow?.move
 		);
--- lib/components/track-changes/test/MoveRangeStart.test.ts
+++ lib/components/track-changes/test/MoveRangeStart.test.ts
@@ -16,6 +16,19 @@ describe('MoveToRangeStart and MoveFromRangeStart elements...', () => {
 		)
 	);
 
+	const moveToRangeStartWithoutAuthor = MoveRangeStart.fromNode(
+		create(
+			`<w:moveToRangeStart xmlns:w="${
+				NamespaceUri.w
+			}" w:id="1" w:date="${date.toISOString()}" w:name="Move_to_1" />`
+		)
+	);
+	const moveToRangeStartWithoutDate = MoveRangeStart.fromNode(
+		create(
+			`<w:moveToRangeStart xmlns:w="${NamespaceUri.w}" w:id="1" w:author="Angel" w:name="Move_to_1" />`
+		)
+	);
+
 	const moveFromRangeStart = MoveRangeStart.fromNode(
 		create(
 			`<w:moveFromRangeStart xmlns:w="${
@@ -23,6 +36,20 @@ describe('MoveToRangeStart and MoveFromRangeStart elements...', () => {
 			}" w:id="1" w:date="${date.toISOString()}" w:author="Angel" w:name="Move_from_1" />`
 		)
 	);
+
+	const moveFromRangeStartWithoutAuthor = MoveRangeStart.fromNode(
+		create(
+			`<w:moveFromRangeStart xmlns:w="${
+				NamespaceUri.w
+			}" w:id="1" w:date="${date.toISOString()}" w:name="Move_from_1" />`
+		)
+	);
+	const moveFromRangeStartWithoutDate = MoveRangeStart.fromNode(
+		create(
+			`<w:moveFromRangeStart xmlns:w="${NamespaceUri.w}" w:id="1" w:author="Angel" w:name="Move_from_1" />`
+		)
+	);
+
 	it('Create MoveToRangeStart from node', () => {
 		expect(moveToRangeStart.props.author).toBe('Gabe');
 		expect(moveToRangeStart.props.date).toBe(date.toISOString());
@@ -31,9 +58,45 @@ describe('MoveToRangeStart and MoveFromRangeStart elements...', () => {
 		expect(moveToRangeStart.props.type).toBe('to');
 	});
 
+	it('Create MoveToRangeStart without author from node', () => {
+		expect(moveToRangeStartWithoutAuthor.props.author).toBe(null);
+		expect(moveToRangeStartWithoutAuthor.props.date).toBe(
+			date.toISOString()
+		);
+		expect(moveToRangeStartWithoutAuthor.props.id).toBe(1);
+		expect(moveToRangeStartWithoutAuthor.props.name).toBe('Move_to_1');
+		expect(moveToRangeStartWithoutAuthor.props.type).toBe('to');
+	});
+
+	it('Create MoveToRangeStart without date from node', () => {
+		expect(moveToRangeStartWithoutDate.props.author).toBe('Angel');
+		expect(moveToRangeStartWithoutDate.props.date).toBe(null);
+		expect(moveToRangeStartWithoutDate.props.id).toBe(1);
+		expect(moveToRangeStartWithoutDate.props.name).toBe('Move_to_1');
+		expect(moveToRangeStartWithoutDate.props.type).toBe('to');
+	});
+
 	it('Create MoveFromRangeStart from node', () => {
 		expect(moveFromRangeStart.props.type).toBe('from');
 		expect(moveFromRangeStart.props.author).toBe('Angel');
+	});
+
+	it('Create MoveFromRangeStart without author from node', () => {
+		expect(moveFromRangeStartWithoutAuthor.props.author).toBe(null);
+		expect(moveFromRangeStartWithoutAuthor.props.date).toBe(
+			date.toISOString()
+		);
+		expect(moveFromRangeStartWithoutAuthor.props.id).toBe(1);
+		expect(moveFromRangeStartWithoutAuthor.props.name).toBe('Move_from_1');
+		expect(moveFromRangeStartWithoutAuthor.props.type).toBe('from');
+	});
+
+	it('Create MoveFromRangeStart without date from node', () => {
+		expect(moveFromRangeStartWithoutDate.props.author).toBe('Angel');
+		expect(moveFromRangeStartWithoutDate.props.date).toBe(null);
+		expect(moveFromRangeStartWithoutDate.props.id).toBe(1);
+		expect(moveFromRangeStartWithoutDate.props.name).toBe('Move_from_1');
+		expect(moveFromRangeStartWithoutDate.props.type).toBe('from');
 	});
 
 	it('Create MoveToRangeStart node from MoveRangeStart object', () => {
@@ -50,7 +113,7 @@ describe('MoveToRangeStart and MoveFromRangeStart elements...', () => {
 				create(
 					`<moveToRangeStart xmlns="${NamespaceUri.w}" xmlns:ns1="${
 						NamespaceUri.w
-					}" ns1:id="2" ns1:name="To_Range_Object" ns1:date="${date.toISOString()}" ns1:author="Gabe" />`
+					}" ns1:id="2" ns1:date="${date.toISOString()}" ns1:author="Gabe" ns1:name="To_Range_Object" />`
 				)
 			)
 		);
@@ -70,7 +133,78 @@ describe('MoveToRangeStart and MoveFromRangeStart elements...', () => {
 				create(
 					`<moveFromRangeStart xmlns="${NamespaceUri.w}" xmlns:ns1="${
 						NamespaceUri.w
-					}" ns1:id="3" ns1:name="From_Range_Object"  ns1:date="${date.toISOString()}" ns1:author="Angel"  />`
+					}" ns1:id="3" ns1:date="${date.toISOString()}" ns1:author="Angel" ns1:name="From_Range_Object" />`
+				)
+			)
+		);
+	});
+
+	it('Create MoveFromRangeStart node MoveRangeStart from object without author and date parameters', () => {
+		const toRangeObject = new MoveRangeStart({
+			id: 3,
+			type: 'from',
+			name: 'From_Range_Object',
+		});
+
+		expect(serialize(toRangeObject.toNode())).toBe(
+			serialize(
+				create(
+					`<moveFromRangeStart xmlns="${NamespaceUri.w}" xmlns:ns1="${NamespaceUri.w}" ns1:id="3" ns1:name="From_Range_Object" />`
+				)
+			)
+		);
+	});
+
+	it('Create MoveFromRangeStart node MoveRangeStart from object without author parameter', () => {
+		const toRangeObject = new MoveRangeStart({
+			id: 3,
+			type: 'from',
+			name: 'From_Range_Object',
+			author: 'Angel',
+		});
+
+		expect(serialize(toRangeObject.toNode())).toBe(
+			serialize(
+				create(
+					`<moveFromRangeStart xmlns="${NamespaceUri.w}" xmlns:ns1="${NamespaceUri.w}" ns1:id="3"  ns1:author="Angel" ns1:name="From_Range_Object" />`
+				)
+			)
+		);
+	});
+
+	it('Create MoveFromRangeStart node MoveRangeStart from object without date parameter', () => {
+		const toRangeObject = new MoveRangeStart({
+			id: 3,
+			type: 'from',
+			name: 'From_Range_Object',
+			date: date,
+		});
+
+		expect(serialize(toRangeObject.toNode())).toBe(
+			serialize(
+				create(
+					`<moveFromRangeStart xmlns="${NamespaceUri.w}" xmlns:ns1="${
+						NamespaceUri.w
+					}" ns1:id="3" ns1:date="${date.toISOString()}" ns1:name="From_Range_Object" />`
+				)
+			)
+		);
+	});
+
+	it('Create MoveToRangeStart node MoveRangeStart from object without date parameter', () => {
+		const toRangeObject = new MoveRangeStart({
+			id: 3,
+			type: 'to',
+			name: 'To_Range_Object',
+			date: date,
+		});
+
+		expect(serialize(toRangeObject.toNode())).toBe(
+			serialize(
+				create(
+					`<moveToRangeStart xmlns="${NamespaceUri.w}" xmlns:ns1="${
+						NamespaceUri.w
+					}" ns1:id="3" ns1:date="${date.toISOString()}" ns1:name="To_Range_Object" />`
 				)
 			)
 		);
