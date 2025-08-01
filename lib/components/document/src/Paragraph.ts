c2b9b4dec71aac3a3e6e7abc56da010df5e50472
Inés Nieto <inieto@rws.com>
2025-08-01 12:36:34 +0200

    Fix

--- lib/components/document/src/Paragraph.ts
+++ lib/components/document/src/Paragraph.ts
@@ -39,7 +39,6 @@ import type { Insertion } from '../../track-changes/src/Insertion.ts';
 import type { Move } from '../../track-changes/src/Move.ts';
 import type { MoveRangeEnd } from '../../track-changes/src/MoveRangeEnd.ts';
 import type { MoveRangeStart } from '../../track-changes/src/MoveRangeStart.ts';
-import type { TextDeletion } from '../../track-changes/src/TextDeletion.ts';
 import type { BookmarkRangeEnd } from './BookmarkRangeEnd.ts';
 import type { BookmarkRangeStart } from './BookmarkRangeStart.ts';
 import type { Field } from './Field.ts';
