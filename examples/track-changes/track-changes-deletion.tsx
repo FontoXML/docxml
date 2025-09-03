53469649ef95e9cb6b8df089665f71742e3120eb
Gabe Webb <gabe.webb@liones.nl>
2025-09-03 13:09:25 +0200

    Modify example file for Luis.

--- examples/track-changes/track-changes-deletion.tsx
+++ examples/track-changes/track-changes-deletion.tsx
@@ -1,5 +1,12 @@
 /** @jsx  Docx.jsx */
-import Docx, { Deletion, Paragraph, Section, Text } from '../../mod.ts';
+import Docx, {
+	Deletion,
+	FootnoteReference,
+	Paragraph,
+	Section,
+	Text,
+	type FootnoteProps,
+} from '../../mod.ts';
 
 // Create a new .docx file with track changes enabled.
 const docxFile = Docx.fromNothing().withSettings({
@@ -8,17 +15,38 @@ const docxFile = Docx.fromNothing().withSettings({
 
 const date = new Date();
 
+const footnoteProps: FootnoteProps = {
+	numberingFormat: 'lowerRoman',
+	position: 'pageBottom',
+	restart: 'continuous',
+};
+
+const footnoteReferenceStyleName = 'FootnoteReference';
+
+// Create a footnote reference that will be deleted.
+const newFootnoteReference = await docxFile.document.footnotes.add(
+	new Paragraph({}, new Text({}, 'Perhaps not so old.')),
+	footnoteReferenceStyleName
+);
+
 // Create a new deleted paragraph
 const testDeletedParagraph = new Paragraph(
 	{ pilcrow: { deletion: { author: 'Ángel', date: date, id: 1 } } },
 	new Deletion(
 		{ author: 'Ángel', date: date, id: 1 },
-		new Text({}, 'This is just a test.')
+		new Text({}, 'This is just a test.'),
+		new FootnoteReference({
+			id: newFootnoteReference,
+			style: 'FootnoteReference',
+		})
 	)
 );
 
 // Create a section as the parent of our new paragraph.
-const testSection = new Section({}, testDeletedParagraph);
+const testSection = new Section(
+	{ footnotes: footnoteProps },
+	testDeletedParagraph
+);
 
 // Set that section as the content of our document.
 docxFile.document.set(testSection);
@@ -28,12 +56,17 @@ await docxFile.toFile('track-changes-deletion.docx');
 
 // Alternatively, you can use JSX:
 await Docx.fromJsx(
-	<Section>
+	<Section footnotes={footnoteProps}>
 		<Paragraph
 			pilcrow={{ deletion: { id: 1, author: 'ines', date: new Date() } }}
 		>
 			<Deletion id={0} author="Ines" date={new Date()}>
-				my old friend.
+				<Text>my old</Text>
+				<FootnoteReference
+					id={newFootnoteReference}
+					style={footnoteReferenceStyleName}
+				/>
+				<Text>friend.</Text>
 			</Deletion>
 		</Paragraph>
 	</Section>
