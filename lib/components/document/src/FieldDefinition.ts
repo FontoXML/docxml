697f179673745e1dd169827b1e4f47494adcc747
Gabe Webb <gabe.webb@liones.nl>
2025-11-13 16:58:08 +0100

    Add field codes.

---
+++ lib/components/document/src/FieldDefinition.ts
@@ -0,0 +1,76 @@
+// type FieldDefinition = {
+// 	name: string;
+// 	argument: string | null;
+// 	fieldSwitch: FieldSwitch | FieldSwitch[];
+// };
+
+import { Component } from '../../../classes/src/Component.ts';
+import { registerComponent } from '../../../utilities/src/components.ts';
+import { create } from '../../../utilities/src/dom.ts';
+
+export type FieldDefinitionChild = never;
+
+export enum FieldNames {
+	hyperlink = 'HYPERLINK',
+	date = 'DATE',
+	error = 'ERROR',
+}
+
+export type FieldDefinitionProps =
+	| {
+			name: FieldNames.hyperlink;
+			value: string;
+			fieldSwitches?: {
+				newWindow?: boolean;
+				locationInFile?: string;
+				screenTip?: string;
+				hyperlinkCoordinates: string;
+				target: string;
+			};
+	  }
+	| {
+			name: FieldNames.date;
+			value: string;
+	  }
+	| {
+			name: FieldNames.error;
+			value: string | null;
+	  };
+
+export class FieldDefinition extends Component<
+	FieldDefinitionProps,
+	FieldDefinitionChild
+> {
+	public static override readonly children: never;
+
+	public static override readonly mixed: boolean = false;
+
+	public override toNode(): Node {
+		return create(
+			`
+			"Hello"
+			`,
+			{
+				name: this.props.name,
+				value: this.props.value,
+			}
+		);
+	}
+
+	static override fromNode(node: Node): FieldDefinition {
+		const textContent = node.textContent?.split(' ');
+		if (textContent) {
+			return new FieldDefinition({
+				name:
+					textContent[0] in FieldNames
+						? (textContent[0] as FieldNames)
+						: FieldNames.error,
+				value: textContent[1] ?? null,
+			});
+		} else {
+			return new FieldDefinition({ name: FieldNames.error, value: null });
+		}
+	}
+}
+
+registerComponent(FieldDefinition);
--- lib/components/document/src/FieldRangeInstruction.ts
+++ lib/components/document/src/FieldRangeInstruction.ts
@@ -1,3 +1,4 @@
+import { FieldDefinition } from '../../../../mod.ts';
 import {
 	Component,
 	type ComponentAncestor,
@@ -17,7 +18,7 @@ import {
 /**
  * A type describing the components accepted as children of {@link FieldRangeInstruction}.
  */
-export type FieldRangeInstructionChild = string;
+export type FieldRangeInstructionChild = string | FieldDefinition;
 
 /**
  * A type describing the props accepted by {@link FieldRangeInstruction}.
