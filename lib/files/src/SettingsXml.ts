43951cdbd8c54f0c1d57c087432361d3394261c1
Inés Nieto <inieto@rws.com>
2026-02-27 16:15:41 +0100

    fix format

--- lib/files/src/SettingsXml.ts
+++ lib/files/src/SettingsXml.ts
@@ -123,9 +123,9 @@ export class SettingsXml extends XmlFileWithContentTypes {
 	public constructor(
 		location: string,
 		relationships: RelationshipsXml = new RelationshipsXml(
-			`${dirname(location)}/_rels/${basename(location)}.rels`,
+			`${dirname(location)}/_rels/${basename(location)}.rels`
 		),
-		settings: Partial<SettingsI> = {},
+		settings: Partial<SettingsI> = {}
 	) {
 		super(location);
 		this.relationships = relationships;
@@ -137,7 +137,7 @@ export class SettingsXml extends XmlFileWithContentTypes {
 	 */
 	public set<Key extends keyof SettingsI>(
 		key: Key,
-		value: SettingsI[Key],
+		value: SettingsI[Key]
 	): void {
 		const meta = settingsMeta.find((meta) => meta.docxmlName === key);
 		if (!meta) {
@@ -147,7 +147,7 @@ export class SettingsXml extends XmlFileWithContentTypes {
 			this.#props[key] = value
 				? (this.relationships.add(
 						meta.ooxmlRelationshipType,
-						value as string,
+						value as string
 					) as SettingsI[Key])
 				: value;
 		} else {
@@ -166,7 +166,7 @@ export class SettingsXml extends XmlFileWithContentTypes {
 		if (meta.ooxmlType === SettingType.Relationship) {
 			return this.#props[key]
 				? (this.relationships.getTarget(
-						this.#props[key] as string,
+						this.#props[key] as string
 					) as SettingsI[Key])
 				: (this.#props[key] as SettingsI[Key]);
 		} else {
@@ -233,7 +233,7 @@ export class SettingsXml extends XmlFileWithContentTypes {
 				}
 			</w:settings>`,
 			this.#props,
-			true,
+			true
 		);
 	}
 
@@ -253,18 +253,18 @@ export class SettingsXml extends XmlFileWithContentTypes {
 	public static override async fromArchive(
 		archive: Archive,
 		contentTypes: ContentTypesXml,
-		location: string,
+		location: string
 	): Promise<SettingsXml> {
 		let relationships;
 
 		const relationshipsLocation = `${dirname(location)}/_rels/${basename(
-			location,
+			location
 		)}.rels`;
 		try {
 			relationships = await RelationshipsXml.fromArchive(
 				archive,
 				contentTypes,
-				relationshipsLocation,
+				relationshipsLocation
 			);
 		} catch (_error: unknown) {
 			// console.error(
@@ -281,12 +281,12 @@ export class SettingsXml extends XmlFileWithContentTypes {
 				"evenAndOddHeaders": docxml:ct-on-off(./${QNS.w}evenAndOddHeaders),
 				"documentProtection": docxml:ct-on-off(./${QNS.w}documentProtection)
 			}`,
-			xml,
+			xml
 		);
 
 		const defaultTabStopTwips = evaluateXPathToNumber(
 			`number(/*/${QNS.w}defaultTabStop/@${QNS.w}val)`,
-			xml,
+			xml
 		);
 		if (defaultTabStopTwips !== null) {
 			settings.defaultTabStop = twip(defaultTabStopTwips);
@@ -295,7 +295,7 @@ export class SettingsXml extends XmlFileWithContentTypes {
 		return new SettingsXml(
 			location,
 			relationships || new RelationshipsXml(relationshipsLocation),
-			settings,
+			settings
 		);
 	}
 }
