1b4ba0df42b095ef28bccb31b29a489e0b01c03c
Inés Nieto <inieto@rws.com>
2026-04-29 16:14:09 +0200

    add tests

--- lib/properties/test/paragraph-properties.test.ts
+++ lib/properties/test/paragraph-properties.test.ts
@@ -114,6 +114,42 @@ describe('Paragraph formatting', () => {
 		);
 	});
 
+	describe('lineRule "exact"', () => {
+		test(
+			`<w:pPr ${ALL_NAMESPACE_DECLARATIONS}>
+				<w:spacing w:line="240" w:lineRule="exact" />
+			</w:pPr>`,
+			{
+				spacing: {
+					before: null,
+					after: null,
+					line: twip(240),
+					lineRule: 'exact',
+					afterAutoSpacing: false,
+					beforeAutoSpacing: false,
+				},
+			}
+		);
+	});
+
+	describe('lineRule "atLeast"', () => {
+		test(
+			`<w:pPr ${ALL_NAMESPACE_DECLARATIONS}>
+				<w:spacing w:line="240" w:lineRule="atLeast" />
+			</w:pPr>`,
+			{
+				spacing: {
+					before: null,
+					after: null,
+					line: twip(240),
+					lineRule: 'atLeast',
+					afterAutoSpacing: false,
+					beforeAutoSpacing: false,
+				},
+			}
+		);
+	});
+
 	describe('Legacy "left"/"right"', () => {
 		test(
 			`<w:pPr ${ALL_NAMESPACE_DECLARATIONS}>
